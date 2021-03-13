ea93bb8a95ebb3ed424483548f8369952628f46e
Semen Sokolov <simionsimion@yandex.ru>
2021-03-14 01:20:20 +0300

    Done project

--- src/author.cpp
+++ src/author.cpp
@@ -12,7 +12,9 @@ Author::Author(const std::string &full_name, int age, Sex sex) {
   if (full_name.empty()) {
     throw std::invalid_argument("Author::full_name must not be empty");
   }
-  // Tip 1: инициализируйте поля
+    age_ = age;
+    sex_ = sex;
+    full_name_ = full_name;
 }
 
 void Author::SetAge(int age) {
@@ -44,3 +46,4 @@ Sex Author::GetSex() const {
 const std::string &Author::GetFullName() const {
   return full_name_;
 }
+
--- src/book_store.cpp
+++ src/book_store.cpp
@@ -5,9 +5,21 @@
 
 // 1. реализуйте функцию ...
 ResizeStorageStatus resize_storage(Book *&storage, int size, int new_capacity) {
-  // здесь мог бы быть ваш разносторонний и многогранный код ...
-  // Tip 1: проведите валидацию аргументов функции
-  // Tip 2: не забудьте высвободить ранее выделенную память под хранилище
+  if (storage == nullptr) {
+      return ResizeStorageStatus::NULL_STORAGE;
+  }
+  if (new_capacity <= size) {
+      return ResizeStorageStatus::INSUFFICIENT_CAPACITY;
+  }
+  if (size < 0) {
+      return  ResizeStorageStatus::NEGATIVE_SIZE;
+  }
+  if (storage != nullptr && new_capacity >= size && size > 0) {
+    auto newStorage = new Book[new_capacity];
+    std::copy(storage, storage+size, newStorage);
+    delete[] storage;
+    storage = newStorage;
+  }
   return ResizeStorageStatus::SUCCESS;
 }
 
@@ -17,24 +29,31 @@ BookStore::BookStore(const std::string &name) : name_{name} {
   if (name.empty()) {
     throw std::invalid_argument("BookStore::name must not be empty");
   }
-
+    name_ = name;
+    storage_capacity_ = kInitStorageCapacity;
+    storage_ = new Book[storage_capacity_];
   // здесь мог бы быть ваш сотрясающий землю и выделяющий память код ...
 }
 
 // 3. реализуйте деструктор ...
 BookStore::~BookStore() {
-  // здесь мог бы быть ваш высвобождающий разум от негатива код ...
-  // Tip 1: я свободен ..., словно память в куче: не забудьте обнулить указатель
+    if (storage_ != nullptr) {
+        delete[] storage_;
+        storage_ = nullptr;
+    }
+    storage_capacity_ = 0;
+    storage_size_ = 0;
 }
 
 // 4. реализуйте метод ...
 void BookStore::AddBook(const Book &book) {
   if (storage_size_ == storage_capacity_) {
-    // здесь мог бы быть ваш умопомрачительный код ...
+        resize_storage_internal(storage_capacity_ + kCapacityCoefficient);
     // Tip 1: используйте функцию resize_storage_internal, задав новый размер хранилища
     // Tip 2: не забудьте обработать статус вызова функции
   }
-  // Tip 3: не забудьте добавить книгу в наше бездонное хранилище ...
+  storage_[storage_size_] = book;
+  storage_size_+=1;
 }
 
 // РЕАЛИЗОВАНО
